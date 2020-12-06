66c6a8434f59f99fac547286ea5506429a288a14
Sun-Yuhe <631463228@qq.com>
2020-12-06 21:39:21 +0800

    first commit

---
+++ Assets/Karting/Scripts/UI/CurrentLevelName.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class CurrentLevelName : MonoBehaviour
+{
+
+    private TextMeshProUGUI t;
+    private string currentLevelName;
+    public GameFlowManager gfm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        t = this.GetComponent<TextMeshProUGUI>();
+        currentLevelName = "level " + gfm.currentId.ToString();
+        t.text = currentLevelName;
+    }
+
+}
---
+++ Assets/Karting/Scripts/UI/LoadSceneButton.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace KartGame.UI
+{
+    public class LoadSceneButton : MonoBehaviour
+    {
+        [Tooltip("What is the name of the scene we want to load when clicking the button?")]
+        public string SceneName;
+        int nextLvId;
+        int CurrentLvId;
+
+        public void LoadTargetScene() 
+        {
+            SceneManager.LoadSceneAsync(SceneName);
+        }
+        public void LoadNextLv()
+        {
+            nextLvId = PlayerPrefs.GetInt("CurrentCompleteLv", 1) + 1;
+            SceneManager.LoadSceneAsync("Lvl" + nextLvId.ToString());
+        }
+        public void LoadCurrentLv()
+        {
+            CurrentLvId = PlayerPrefs.GetInt("CurrentCompleteLv", 1);
+            SceneManager.LoadSceneAsync("Lvl" + CurrentLvId.ToString());
+        }
+    }
+}
