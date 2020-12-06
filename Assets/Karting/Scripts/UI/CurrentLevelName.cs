14779f7d92c2353f972be9625d0f843b57aef082
Sun-Yuhe <631463228@qq.com>
2020-12-06 22:19:33 +0800

    add engine assignment 10 contents

--- Assets/Karting/Scripts/UI/CurrentLevelName.cs
+++ Assets/Karting/Scripts/UI/CurrentLevelName.cs
@@ -6,13 +6,13 @@ public class CurrentLevelName : MonoBehaviour
 
     private TextMeshProUGUI t;
     private string currentLevelName;
-    public GameFlowManager gfm;
+    public GameFlowManager gameManager;
 
     // Start is called before the first frame update
     void Start()
     {
         t = this.GetComponent<TextMeshProUGUI>();
-        currentLevelName = "level " + gfm.currentId.ToString();
+        currentLevelName = "level " + gameManager.currentId.ToString();
         t.text = currentLevelName;
     }
 
